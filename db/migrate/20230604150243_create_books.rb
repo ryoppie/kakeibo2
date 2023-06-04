a2e4aab896fba5e202e55380ab9edb707e532e9b
ryoppie <rmsidejob0913@gmail.com>
2023-06-05 00:05:06 +0900

    マイグレーション

--- db/migrate/20230604150243_create_books.rb
+++ db/migrate/20230604150243_create_books.rb
@@ -1,7 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-
+      t.integer :expend
+      t.string  :category
+      t.integer :year
+      t.integer :month
+      t.integer :day
+      t.integer :amount
       t.timestamps
     end
   end
